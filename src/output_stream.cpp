866ab207172abaf088c41d0a3ddedd1177f22700
bitshift <bitshift@posteo.org>
2017-02-20 01:30:29 +0100

    Adds OpenCV video writer

--- src/output_stream.cpp
+++ src/output_stream.cpp
@@ -10,6 +10,8 @@ output_stream::output_stream()
     , work_(new boost::asio::io_service::work(*io_service_))
 {
     threads_.create_thread(boost::bind(&output_stream::run, this));
+    video_.open("gl_mixer_stream.avi", 
+        cv::VideoWriter::fourcc('H','2','6','4'), 60, cv::Size(640, 480), true);
 }
 
 output_stream::~output_stream()
@@ -26,6 +28,15 @@ void output_stream::post(const QImage& image)
 void output_stream::write(const QImage& image)
 {
     auto bits = image.bits();
+    auto matrix = cv::Mat(640, 480, CV_8UC1, *bits);
+
+    // cv::namedWindow( "image", 1 );
+    cv::imshow("image", matrix);
+
+    if (video_.isOpened())
+    {    
+        video_ << matrix;
+    }
 }
 
 void output_stream::run()
--- src/output_stream.h
+++ src/output_stream.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -22,10 +26,12 @@ private:
     void log(const std::string message) const;
     void run();
     
-    void write(const QImage& image);    
+    void write(const QImage& image);
 
-    std::shared_ptr<boost::asio::io_service>        io_service_;
-    std::shared_ptr<boost::asio::io_service::work>  work_;
+    std::shared_ptr<boost::asio::io_service> io_service_;
+    std::shared_ptr<boost::asio::io_service::work> work_;
 
-    boost::thread_group         threads_;
+    boost::thread_group threads_;
+
+    cv::VideoWriter video_;
 };
