dfbbdb6c79b909c8a569d2f1c2415fe2b610d180
bitshift <bitshift@posteo.org>
2017-02-21 19:42:30 +0100

    Implements counter in shared memory and resolves RAII issues

--- src/shm_store.cpp
+++ src/shm_store.cpp
@@ -3,12 +3,20 @@
 #include <iostream>
 #include <qdebug.h>
 
+using namespace boost::interprocess;
+
 boost::mutex g_stream_lock;
 
 shm_store::shm_store()
     : io_service_(new boost::asio::io_service)
     , work_(new boost::asio::io_service::work(*io_service_))
+    , counter_(0)
 {
+    shared_memory_object::remove("glmtexture");
+
+    segment_.reset(new managed_shared_memory(create_only, "glmtexture", 65536));
+    texture_ = segment_->construct<size_t>("tex")(0);
+
     threads_.create_thread(boost::bind(&shm_store::run, this));
 }
 
@@ -16,6 +24,8 @@ shm_store::~shm_store()
 {
     work_.reset();
 	threads_.join_all();
+
+    shared_memory_object::remove("glmtexture");
 }
 
 void shm_store::post(const QImage& image)
@@ -31,15 +41,12 @@ void shm_store::write(const QImage& image)
                    const_cast<uchar*>(image.bits()), 
                    image.bytesPerLine()).clone();
     
-
+    *texture_ = counter_;
+    counter_++;
+    // cv::imshow("image", matrix);
 }
 
 void shm_store::run()
 {
     io_service_->run();
 }
-
-
-
-
-
--- src/shm_store.h
+++ src/shm_store.h
@@ -11,8 +11,11 @@
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
 
 #include <QImage>
+
+using namespace boost::interprocess;
 
 class shm_store {
 
@@ -21,7 +24,7 @@ public:
     ~shm_store();
 
     void post(const QImage& image);
-    
+
 private:
     void run();    
     void write(const QImage& image);
@@ -30,4 +33,9 @@ private:
     std::shared_ptr<boost::asio::io_service::work> work_;
 
     boost::thread_group threads_;
+
+    std::shared_ptr<managed_shared_memory> segment_;
+
+    size_t* texture_;
+    size_t counter_;
 };
